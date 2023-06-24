1364c819c5ebea7add397e512734bd3101929285
mohammadborabadi0 <www.mohammadborabadi0@gmail.com>
2023-06-24 14:12:47 +0330

    test2 commit

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -24,6 +24,42 @@ const Section2 = () => {
             </span>
           </div>
         </div>
+        <div className="border px-6 py-2">
+          <h4 className="font-extrabold text-lg">Invoices Overduce</h4>
+          <div className="flex justify-between items-center">
+            <span>6</span>
+            <span className="text-lg bg-green-600 text-white px-2 py-1 rounded-lg">
+              +2.4%
+            </span>
+          </div>
+        </div>
+        <div className="border px-6 py-2">
+          <h4 className="font-extrabold text-lg">Invoices Overduce</h4>
+          <div className="flex justify-between items-center">
+            <span>6</span>
+            <span className="text-lg bg-green-600 text-white px-2 py-1 rounded-lg">
+              +2.4%
+            </span>
+          </div>
+        </div>
+        <div className="border px-6 py-2">
+          <h4 className="font-extrabold text-lg">Invoices Overduce</h4>
+          <div className="flex justify-between items-center">
+            <span>6</span>
+            <span className="text-lg bg-green-600 text-white px-2 py-1 rounded-lg">
+              +2.4%
+            </span>
+          </div>
+        </div>
+        <div className="border px-6 py-2">
+          <h4 className="font-extrabold text-lg">Invoices Overduce</h4>
+          <div className="flex justify-between items-center">
+            <span>6</span>
+            <span className="text-lg bg-green-600 text-white px-2 py-1 rounded-lg">
+              +2.4%
+            </span>
+          </div>
+        </div>
       </section>
       <div className="col-span-2 row-span-2 rounded-lg py-2">
         <AreaChartComponent />
